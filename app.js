ad93f452b0c5812bd85705aa825b655ae1cab8b3
ki kang <kikang20@yahoo.com>
2024-03-18 18:39:22 -0700

    whatttt

---
+++ app.js
@@ -0,0 +1,25 @@
+function onSearch(names) {
+    const search = document.getElementById("dinoSearch").value.toLowerCase();
+    const results = document.getElementById("search-result");
+    
+    const filtered = names.filter((name) => {
+        if (!search) {
+            name.doesMatch = true;
+            return name;
+        } else {
+            const dinosaurs = name.name.toLowerCase();
+            const nameMatch = dinosaurs.includes(search);
+            name.doesMatch = nameMatch;
+            return nameMatch;
+        }
+    });
+    
+    console.log(filtered); 
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const searchInput = document.getElementById("dinoSearch");
+    searchInput.addEventListener("input", function() {
+        onSearch(dinosaurs);
+    });
+});
