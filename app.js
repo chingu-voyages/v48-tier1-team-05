0295037e81b70f69d520a538e93f56d7adee6197
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2024-04-01 09:12:36 -0700

    move graph.js into app.js

--- app.js
+++ app.js
@@ -148,6 +148,29 @@ function countDinosaursPerCountry(dinosaurs) {
   return countryObject
 }
 
+
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3],
+        borderWidth: 1,
+        hoverOffset: 4
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
 // on hover, the event.target.id is a country code
 const countryCodes = {
   AQ: 'Antarctica',
